4aaf2bc387f8e91eea6b68f315a78863695265ae
soofyane <soufianehalloumi33@gmail.com>
2023-08-17 07:05:41 -0700

    mock up of the firs, second and third task simple shell

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * main - A custom implementation of the sh (bourne shell) in C
  *
@@ -8,29 +7,57 @@
 int main(void)
 {
 
-	char *lineptr;
-	size_t n = 0;
-	ssize_t nchars_read;
+        char *lineptr;
+        size_t n = 0;
+        ssize_t nchars_read;
+        char *arr[20];
+	char *token;
+        int i, st;
+        pid_t pid;
+	
+        /* arr = malloc(sizeof(char *) *1024);*/
+        i = 0;
+        while (1 == 1)
+        {
+                handle_string("student@alxafrica.com $➜  ");
+                nchars_read = getline(&lineptr, &n, stdin);
+                if (nchars_read == -1)
+                {
+                        handle_string("Exiting shell ...\n");
+                        return (-1);
+                }
 
-	while (1 == 1)
-	{
-		handle_string("student@alxafrica.com $➜  ");
-		nchars_read = getline(&lineptr, &n, stdin);
-		if (nchars_read == -1)
-		{
-			handle_string("Exiting shell ...");
-			return (-1);
-		}
+                else if ( nchars_read == 1)
+                {
+                        handle_string(lineptr);
+                        continue;
+                }
+                else
+                {
+                       	token = strtok(lineptr, " \t\n");
+                        while (token != NULL)
+                        {
+                                arr[i] = token;
+                                token = strtok(NULL, " \t\n");
+                                i++;
+                        }
+                        arr[i] = NULL;
+                        pid = fork();
 
-		else if ( nchars_read == 1)
-			handle_string( lineptr);
-		else
-		{
-			handle_string(lineptr);
-			handle_string("\n");
-		}
-	}
-	free(lineptr);
-	return (0);
+                        if (pid == 0)
+                        {
+                                if (execve(arr[0], arr, NULL) == -1)
+                                {
+                                        perror("Error:");
+                                        return (-1);
+                                }
+                        }
+                        else
+                                wait(&st);
+                       /*free(arr);*/
+                        i = 0;
+                }
+        }
+        free(lineptr);
+        return (0);
 }
-
--- main.h
+++ main.h
@@ -1,10 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <strings.h>
+#include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int _putchar(char c);
 void _puts(char *str);
